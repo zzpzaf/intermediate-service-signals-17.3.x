e44813a134770dc7999d96a99f3d9d759837fd30
Panos Zafeiropoulos <zzpzaf@gmail.com>
2024-07-14 10:18:30 +0200

    Using Signals with a shared Service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Test1Component } from './test1/test1.component';
+import { Test2Component } from './test2/test2.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Test1Component, Test2Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'intermediate-service-signals-17.3.x.1';
+  public compName = this.constructor.name.replace('_','');
+  constructor() {}
+
+  // title = 'signals1';
+  title = 'Angular 17.x Application: intermediate-service using signals';
 }
--- src/app/test2/test2.component.ts
+++ src/app/test2/test2.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, effect } from '@angular/core';
+import { IMsg, ShareDataService } from '../share-data.service';
 
 @Component({
-  selector: 'app-test2',
+  selector: 'test2',
   standalone: true,
   imports: [],
   templateUrl: './test2.component.html',
@@ -9,4 +10,45 @@ import { Component } from '@angular/core';
 })
 export class Test2Component {
 
+    @ViewChild('input') inputMessageText!: ElementRef;
+
+    // private subscription!: Subscription ;
+    public msgReceived: string = ''; 
+    public msgSent : string = ''; 
+    public compName = this.constructor.name.replace('_', '');
+    public msg: IMsg = {sender: '', msg: ''};
+  
+    constructor(private sharedService: ShareDataService,) {
+  
+      effect(()=> {
+        this.msg = this.sharedService.getData();
+        console.log('>===>> Test2 - Messgae Changed/Received:', this.msg);
+        if (this.msg.sender != this.compName) this.msgReceived = this.msgReceived + this.msg.msg + '<br>';
+      });
+  
+    }
+    
+
+    onClick(msgToBeSent:string) {
+      if (msgToBeSent === '') return;
+      console.log( '>===>> ' + this.compName  + ' - '  + 'onClick - '+  'Sending msg to sharedService:' + ' - ' + msgToBeSent);
+      this.sendMessgae(msgToBeSent);
+  
+    }
+  
+    onKeyEnter(event: any) {
+      let msgTxt: string = event.target.value;
+      if (event.key === 'Enter' && msgTxt != '') {
+        console.log( '>===>> ' + this.compName  + ' - '  + 'onKeyEnter - '+  'Sending msg to sharedService:' + ' - ' + event.target.value);
+        this.sendMessgae(msgTxt);
+      }
+    }
+  
+    sendMessgae(msgTxt:string) {
+      this.sharedService.setData({sender: this.compName, msg: msgTxt});
+      this.msgSent = this.msgSent +msgTxt + '<br>';
+      this.inputMessageText.nativeElement.value = '';
+    }
+
 }
+
